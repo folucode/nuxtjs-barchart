74b5c1fa852d5c38e0ed8a3564a9544ece781c7e
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-05-21 13:01:47 +0100

    initial commit

---
+++ components/BarChart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <canvas id="bar-chart"></canvas>
+    </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js';
+import barChartData from "../data/chart-data";
+barChartData
+
+export default {
+    name: "BarChart",
+    data() {
+        return {
+            barChartData
+        }
+    },
+    mounted() {
+        const ctx = document.getElementById("bar-chart");
+        Chart.register(...registerables);
+        new Chart(ctx, this.barChartData);
+    }
+};
+</script>
